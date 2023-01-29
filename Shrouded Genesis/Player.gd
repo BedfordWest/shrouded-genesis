ccfbd7c8d4ef8c374d760634c8b09372bf18dffe
Andy Delso <andy@delso.me>
2023-01-28 21:39:32 -0600

    allow player to run using accel and deccel

--- Shrouded Genesis/Player.gd
+++ Shrouded Genesis/Player.gd
@@ -15,28 +15,42 @@ func _physics_process(delta):
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+	
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+		
 	if Input.is_action_pressed("move_back"):
 		# Notice how we are working with the vector's x and z axes.
 		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
+		
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
-		
-	# Ground velocity
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
-	# Vertical velocity
-	velocity.y -= fall_acceleration * delta
 	
 	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y += jump_impulse
 		
+	updateGroundVelocity(direction, delta)
+	updateVerticalVelocity(delta)
+		
 	# Moving the character
 	velocity = move_and_slide(velocity, Vector3.UP)
+
+func updateGroundVelocity(direction, delta):
+	if Input.is_action_just_pressed("run"):
+		speed /= 18 * delta
+		
+	if Input.is_action_just_released("run"):
+		speed *= 18 * delta
+	# Ground velocity
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+func updateVerticalVelocity(delta):
+	# Vertical velocity
+	velocity.y -= fall_acceleration * delta
